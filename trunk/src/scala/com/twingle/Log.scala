cf8b15342f6ddf342bce74f1f577a72af51914a4
samskivert <samskivert@47fd925a-9d4c-11dd-87e9-a30ea5446288>
2009-04-05 22:16:18 +0000

    A simple log shim, a simple daemon entry point and script to run it.

---
+++ trunk/src/scala/com/twingle/Log.scala
@@ -0,0 +1,19 @@
+//
+// $Id$
+
+package com.twingle
+
+import java.util.logging.Logger
+
+/**
+ * Provides a logger used by the entire project.
+ */
+object Log
+{
+  /** Statically import this into your class and use it like so:
+   * <pre>
+   * log.warning("Oh my god, the Canadians are coming!", "radar", radarReading())
+   * </pre>
+   */
+  val log = Logger.getLogger("com.twingle")
+}
---
+++ trunk/src/scala/com/twingle/daemon/Twingled.scala
@@ -0,0 +1,16 @@
+//
+// $Id$
+
+package com.twingle.daemon
+
+import com.twingle.Log.log
+
+/**
+ * The main entry point for the Twingle daemon.
+ */
+object Twingled
+{
+  def main (args :Array[String]) {
+    log.info("Twingle daemon running.")
+  }
+}
