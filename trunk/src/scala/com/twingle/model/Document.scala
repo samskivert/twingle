086a67c7110ab5798583835e76f487885f9fd2f3
samskivert <samskivert@47fd925a-9d4c-11dd-87e9-a30ea5446288>
2009-04-06 04:29:41 +0000

    Tidied up the builder, added more tests.

--- trunk/src/scala/com/twingle/model/Document.scala
+++ trunk/src/scala/com/twingle/model/Document.scala
@@ -14,43 +14,35 @@ import com.twingle.persist.DatabaseObject
 class Document extends AnyRef with DatabaseObject
 {
   /** The URL defining this document's location or "<unknown>". */
-  def location () :String = optA(stringM, "location") | "<unknown>"
+  def location :String = optA(stringM, "location") | "<unknown>"
 
   /** The name of this document or "<unknown>". */
-  def name () :String = optA(stringM, "name") | "<unknown>"
+  def name :String = optA(stringM, "name") | "<unknown>"
 
   /** The text of this document. */
-  def text () :Option[String] = optA(stringM, "text").data
+  def text :Option[String] = optA(stringM, "text").data
 
   /** This document's binary data. */
-  def bits () :Option[ByteBuffer] = optA(byteBufferM, "bits").data
+  def bits :Option[ByteBuffer] = optA(byteBufferM, "bits").data
 
   /** The date on which this document was created. */
-  def created () :Option[Date] = optA(dateM, "created").data
+  def created :Option[Date] = optA(dateM, "created").data
 
   /** The date on which this document was last modified. */
-  def lastModified () :Option[Date] = optA(dateM, "last_modified").data
+  def lastModified :Option[Date] = optA(dateM, "last_modified").data
 }
 
 object Document
 {
-  class Builder extends DatabaseObject.Builder {
+  def builder = new DatabaseObject.Builder {
     def location (location :String) = { add("location", location); this }
     def name (name :String) = { add("name", name); this }
     def text (text :String) = { add("text", text); this }
     def bits (bits :ByteBuffer) = { add("bits", bits); this }
     def created (created :Date) = { add("created", created); this }
-    def lastModified (lastModified :Date) = { add("lastModified", lastModified); this }
+    def lastModified (lastModified :Date) = { add("last_modified", lastModified); this }
     def build :Document = build(new Document)
   }
-
-  def builder = new Builder
-
-  //   def make (location :Option[String], name :Option[String], text :Option[String],
-//             bits :Option[ByteBuffer], created :Option[Date], lastModified :Option[Date]) :Document =
-//     DatabaseObject.builder.add("location", location).add("name", name).add("text", text).
-//       add("bits", bits).add("created", created).add("lastModified", lastModified).
-//       build(new Document)
 }
 
 package tests {
@@ -58,16 +50,21 @@ package tests {
 
   class DocumentSuite extends Suite {
     def testBuilder () {
+      val now = new Date
       val doc = Document.builder.
         location("http://www.samskivert.com/").
         name("MDB's Blog").
         text("Foo bar baz!").
-        created(new Date()).
-        lastModified(new Date()).
+        created(now).
+        lastModified(now).
         build
 
-      Console.println(doc.created)
-      expect("Foo bar baz!") { doc.text }
+      expect("http://www.samskivert.com/") { doc.location }
+      expect("MDB's Blog") { doc.name }
+      expect(Some("Foo bar baz!")) { doc.text }
+      expect(None) { doc.bits }
+      expect(Some(now)) { doc.created }
+      expect(Some(now)) { doc.lastModified }
     }
   }
 }
