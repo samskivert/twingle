ac953745439bc59b6cc859a92fbf5f058d849147
lemurware <lemurware@47fd925a-9d4c-11dd-87e9-a30ea5446288>
2009-04-06 03:52:25 +0000

    Finish up initial twitter status parsing.

--- trunk/src/scala/com/twingle/spider/TwitterClient.scala
+++ trunk/src/scala/com/twingle/spider/TwitterClient.scala
@@ -1,16 +1,43 @@
 package com.twingle.spider
 
+import java.text.SimpleDateFormat
+import java.util.Date
+
 import scala.xml.{Node, XML}
 
 import org.apache.commons.httpclient.auth.AuthScope
 import org.apache.commons.httpclient.cookie.CookiePolicy
 import org.apache.commons.httpclient.methods.GetMethod
-import org.apache.commons.httpclient.{HttpClient, HttpMethod, HttpMethodBase, UsernamePasswordCredentials}
+import org.apache.commons.httpclient.{HttpClient, HttpMethod, HttpMethodBase,
+                                      UsernamePasswordCredentials}
 
 import com.twingle.Log.log
 
 class TwitterClient {
-  def friendsTimeline (username :String, password :String) :Node = {
+  /**
+   * Simple status record to contain a tweet.
+   */
+  class Status {
+    var createdAt :Date = null
+    var tweetId :Int = 0
+    var text :String = null
+    var userId :Int = 0
+    var userName :String = null
+    var screenName :String = null
+
+    override def toString () = {
+      val buf :StringBuffer = new StringBuffer
+      buf.append("[createdAt=").append(createdAt)
+      buf.append(", tweetId=").append(tweetId)
+      buf.append(", text=").append(text)
+      buf.append(", userId=").append(userId)
+      buf.append(", userName=").append(userName)
+      buf.append(", screenName=").append(screenName)
+      buf.toString
+    }
+  }
+
+  def friendsTimeline (username :String, password :String) :Seq[Status] = {
     val url = "http://twitter.com/statuses/friends_timeline.xml"
     val (method, result, responseBody) = httpGet(url, username, password)
     if (result != 200) {
@@ -19,7 +46,13 @@ class TwitterClient {
       return null
     }
 
-    XML.loadString(responseBody)
+    val doc = XML.loadString(responseBody)
+    if (doc == null) {
+      return null
+    }
+    
+    val statuses = (doc \\ "status")
+    statuses.map(parseStatusElement(_))
   }
 
   protected def httpGet (url :String, username :String, password :String) = {
@@ -39,4 +72,23 @@ class TwitterClient {
     (method, result, responseBody)
   }
 
+  protected def parseStatusElement (e :Node) :Status = {
+    val status = new Status
+
+    status.createdAt =
+      TwitterClient.dateFormatter.parse((e \ "created_at").text)
+    status.tweetId = (e \ "id").text.toInt
+    status.text = (e \ "text").text
+
+    val userElem = (e \ "user")
+    status.userId = (userElem \ "id").text.toInt
+    status.userName = (userElem \ "name").text
+    status.screenName = (userElem \ "screen_name").text
+
+    status
+  }
+}
+
+object TwitterClient {
+  val dateFormatter = new SimpleDateFormat("E MMM dd HH:mm:ss Z yyyy")
 }
