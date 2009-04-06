d54029e1a961a551dcced74b23c20bed08054fda
samskivert <samskivert@47fd925a-9d4c-11dd-87e9-a30ea5446288>
2009-04-06 03:04:59 +0000

    Attribute revamp! Less type system insanity. Still some fiddly business to be resolved with how to get the underlying data to a DatabaseObject.

--- src/scala/com/twingle/model/Document.scala
+++ src/scala/com/twingle/model/Document.scala
@@ -7,7 +7,6 @@ import java.nio.ByteBuffer
 import java.util.Date
 
 import com.twingle.persist.DatabaseObject
-import com.twingle.persist.OptAttr
 
 /**
  * Contains data for a document stored in the database.
@@ -15,26 +14,28 @@ import com.twingle.persist.OptAttr
 class Document extends AnyRef with DatabaseObject
 {
   /** The URL defining this document's location or "<unknown>". */
-  def location () :String =
-    attr[String, OptAttr[String]](classOf[OptAttr[String]], "location") | "<unknown>"
+  def location () :String = optA(stringM, "location") | "<unknown>"
 
   /** The name of this document or "<unknown>". */
-  def name () :String =
-    attr[String, OptAttr[String]](classOf[OptAttr[String]], "name") | "<unknown>"
+  def name () :String = optA(stringM, "name") | "<unknown>"
 
   /** The text of this document. */
-  def text () :Option[String] =
-    attr[String, OptAttr[String]](classOf[OptAttr[String]], "text").data
+  def text () :Option[String] = optA(stringM, "text").data
 
   /** This document's binary data. */
-  def bits () :Option[ByteBuffer] =
-    attr[ByteBuffer, OptAttr[ByteBuffer]](classOf[OptAttr[ByteBuffer]], "bits").data
+  def bits () :Option[ByteBuffer] = optA(byteBufferM, "bits").data
 
   /** The date on which this document was created. */
-  def created () :Option[Date] =
-    attr[Date, OptAttr[Date]](classOf[OptAttr[Date]], "created").data
+  def created () :Option[Date] = optA(dateM, "created").data
 
   /** The date on which this document was last modified. */
-  def lastModified () :Option[Date] =
-    attr[Date, OptAttr[Date]](classOf[OptAttr[Date]], "last_modified").data
+  def lastModified () :Option[Date] = optA(dateM, "last_modified").data
+}
+
+object Document
+{
+//   def make (location :Option[String], name :Option[String], text :Option[String],
+//             bits :Option[ByteBuffer], created :Option[Date], lastModified :Option[Date]) :Document
+//   {
+//   }
 }
--- src/scala/com/twingle/persist/DatabaseObject.scala
+++ src/scala/com/twingle/persist/DatabaseObject.scala
@@ -3,6 +3,8 @@
 
 package com.twingle.persist
 
+import java.nio.ByteBuffer
+import java.util.Date
 import java.util.UUID
 
 /**
@@ -11,23 +13,138 @@ import java.util.UUID
  */
 trait DatabaseObject
 {
+  // annoying! why do I have to do this?
+  import DatabaseObject.Marshaler
+
   /** This object's unique identifier. */
-  def id :UUID = attr[UUID, ReqAttr[UUID]](classOf[ReqAttr[UUID]], "id").data
+  def id :UUID = reqA(uuidM, "id").data
 
   /**
-   * Returns a list of all attributes contained by this object.
+   * Returns the names of all attributes contained in this object.
    */
-  def attrs () :List[Attribute[Any]] = throw new Error()
+  def attrs () :Iterator[String] = _attrs.keys
 
   /**
    * Returns true if this object contains an attribute with the specified name.
    */
-  def hasAttr (name :String) :Boolean = throw new Error()
+  def hasAttr (name :String) :Boolean = _attrs.contains(name)
 
   /**
-   * Returns the specified attribute or a blank attribute if this object does
-   * not contain the attribute in question. TODO: if it contains an attribute
-   * of the same name but the wrong type, do we return a blank attribute or fail?
+   * Returns the string value of the supplied field.
    */
-  def attr[A, B <: Attribute[A]] (aclass :Class[B], name :String) :B = throw new Error()
+  protected def value (name :String) :Option[String] = _attrs.get(name)
+
+  /** Represents a single attribute of a database object. */
+  protected abstract class Attr[T] (marsh :Marshaler[T], name :String) {
+    /** Defines the type used as a container for our attribute, e.g. Option, List, etc. */
+    type Container
+
+    /** The data for this attribute, wrapped in its container. */
+    def data :Container
+
+    /** Returns the data for this attribute or the supplied default if attr is not defined. */
+    def | (default: T) :T
+  }
+
+  /** Represents a required attribute. */
+  protected class ReqAttr[T] (marsh :Marshaler[T], name :String) extends Attr[T](marsh, name) {
+    /** Required attributes have no container as they always exist. */
+    type Container = T
+
+    /** Returns the data for this attribute. Throws an exception if it does not exist. */
+    def | (default :T) :T = data
+
+    // from Attr
+    override def data :T = marsh.unmarshal(value(name).get)
+  }
+
+  /** Creates a required attribute. */
+  protected def reqA[T] (marsh :Marshaler[T], name :String) = new ReqAttr[T](marsh, name)
+
+  /** Represents an optional attribute. */
+  protected class OptAttr[T] (marsh :Marshaler[T], name :String) extends Attr[T](marsh, name) {
+    /** Define our container type. In this case an Option. */
+    type Container = Option[T]
+
+    /** Returns the value of this attribute or the supplied default. */
+    def | (default :T) :T = data.getOrElse(default)
+
+    // from Attr
+    override def data :Option[T] = value(name) match {
+      case None => None
+      case Some(text) => Some(marsh.unmarshal(text))
+    }
+  }
+
+  /** Creates an optional attribute. */
+  protected def optA[T] (marsh :Marshaler[T], name :String) = new OptAttr[T](marsh, name)
+
+//   /** Creates an attribute for an optional attribute of a database object. */
+//   protected [T] def listA :Attr[T] = new Attr[T]() {
+//     /** List attribute data is wrapped in a List. */
+//     type Container = List[T]
+
+//     /** Returns the head of our list or the default if the list is empty. */
+//     def | (default :T) :T = if (data.isEmpty) default else data.head
+
+//     def data :List[T] = match (source) {
+//       case None => Empty
+//       case Some(text) => // TODO
+//   }
+
+  // this sucks, why aren't values and methods from DatabaseObject in scope in my trait?
+  protected def uuidM = DatabaseObject.uuidM
+  protected def stringM = DatabaseObject.stringM
+  protected def intM = DatabaseObject.intM
+  protected def dateM = DatabaseObject.dateM
+  protected def byteBufferM = DatabaseObject.byteBufferM
+
+  // I don't like this, need to figure out something better
+  private[persist] def init (attrs :Map[String, String]) {
+    _attrs = attrs
+  }
+
+  private[this] var _attrs :Map[String, String] = null
+}
+
+object DatabaseObject
+{
+  /** Used to convert strings to and from target data types. */
+  protected trait Marshaler[T] {
+    /** Marshals a value for this attribute into a string representation. */
+    def marshal (value :T) :String
+
+    /** Unmarshals a value for this attribute from its underlying string representation. */
+    def unmarshal (value :String) :T
+  }
+
+  /** Creates a marshaler for {@link UUID} fields. */
+  protected val uuidM :Marshaler[UUID] = new Marshaler[UUID] {
+    def marshal (value :UUID) = value.toString
+    def unmarshal (value :String) = UUID.fromString(value)
+  }
+
+  /** Creates a marshaler for {@link String} fields. */
+  protected val stringM :Marshaler[String] = new Marshaler[String] {
+    def marshal (value :String) = value
+    def unmarshal (value :String) = value
+  }
+
+  /** Creates a marshaler for Int fields. */
+  protected val intM :Marshaler[Int] = new Marshaler[Int] {
+    def marshal (value :Int) = value.toString
+    def unmarshal (value :String) = java.lang.Integer.parseInt(value)
+  }
+
+  /** Creates a marshaler for {@link Date} fields. */
+  protected val dateM :Marshaler[Date] = new Marshaler[Date] {
+    def marshal (value :Date) = value.getTime.toString
+    def unmarshal (value :String) = new Date(java.lang.Long.parseLong(value))
+  }
+
+  /** Creates a marshaler for {@link ByteBuffer} fields. */
+  protected val byteBufferM :Marshaler[ByteBuffer] = new Marshaler[ByteBuffer] {
+    def marshal (value :ByteBuffer) = new String(value.array, "ISO-8859-1")
+    def unmarshal (value :String) = ByteBuffer.wrap(value.getBytes("ISO-8859-1"))
+  }
 }
