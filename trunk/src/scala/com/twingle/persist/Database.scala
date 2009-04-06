06a61c1c7833db8e9a67a4e7b7eb12371a96b1c2
samskivert <samskivert@47fd925a-9d4c-11dd-87e9-a30ea5446288>
2009-04-06 00:58:30 +0000

    Behold! An extremely simple database API. To be complexified later.

--- trunk/src/scala/com/twingle/persist/Database.scala
+++ trunk/src/scala/com/twingle/persist/Database.scala
@@ -12,7 +12,17 @@ trait Database
 {
   /**
    * Loads the object with the specified id into an instance of the supplied DatabaseObject derived
-   * class. Returns null if no object exists with the supplied id.
+   * class.
    */
-  def load[C <: DatabaseObject] (oclass :Class[C], id :UUID) :DatabaseObject
+  def load[C <: DatabaseObject] (oclass :Class[C], id :UUID) :Option[C]
+
+  /**
+   * Stores the supplied object in the database. Returns the newly assigned UUID for the object.
+   */
+  def store (obj :DatabaseObject) :UUID
+
+  /**
+   * Searches the database and returns all objects that "match" the supplied query.
+   */
+  def find (query :String) :List[DatabaseObject]
 }
