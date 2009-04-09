1fb02207593f1ae76c9f57ac24e8e80d583e9472
samskivert <samskivert@47fd925a-9d4c-11dd-87e9-a30ea5446288>
2009-04-09 06:47:08 +0000

    Converted everything to new database-based Config stuff, scalafied things a bit along the way.

--- src/scala/com/twingle/spider/FriendFeedSpider.scala
+++ src/scala/com/twingle/spider/FriendFeedSpider.scala
@@ -10,75 +10,6 @@ import scala.xml.{Node, XML}
 
 import com.twingle.Log.log
 
-/** Details a single user's FriendFeed configuration. */
-case class FriendFeedSpiderConfig (val username :String, val remoteKey :String)
-  extends SpiderConfig {
-  protected def toString (buf :StringBuffer) = {
-    buf.append("username=").append(username)
-    buf.append(", remoteKey=").append(remoteKey)
-  }
-}
-
-/** Describes a service for which posts may be seen in a user's FriendFeed feed. */
-case class FriendFeedService (val profileUrl :String,
-                              val iconUrl :String,
-                              val entryType :String,
-                              val name :String) {
-  override def toString () = {
-    val buf :StringBuffer = new StringBuffer
-    buf.append("[profileUrl=").append(profileUrl)
-    buf.append(", iconUrl=").append(iconUrl)
-    buf.append(", entryType=").append(entryType)
-    buf.append(", name=").append(name)
-    buf.append("]").toString
-  }
-}
-
-/** Describes a FriendFeed user's profile. */
-case class FriendFeedProfile (val profileUrl :String,
-                              val nickname :String,
-                              val ffId :String,
-                              val name :String) {
-  override def toString () = {
-    val buf :StringBuffer = new StringBuffer
-    buf.append("[profileUrl=").append(profileUrl)
-    buf.append(", nickname=").append(nickname)
-    buf.append(", ffId=").append(ffId)
-    buf.append(", name=").append(name)
-    buf.append("]").toString
-  }
-}
-
-/** Describes a single post in a user's FriendFeed feed. */
-// TODO: anonymous, hidden, id(internal)
-case class FriendFeedEntry (val updated :Date, 
-                            val service :FriendFeedService,
-                            val title :String,
-                            val link :String,
-                            val published :Date,
-                            val ffId :String,
-                            val userProfile :FriendFeedProfile)
-{
-  override def toString () = {
-    val buf :StringBuffer = new StringBuffer
-    buf.append("[updated=").append(updated)
-    buf.append(", service=").append(service)
-    buf.append(", title=").append(title)
-    buf.append(", link=").append(link)
-    buf.append(", published=").append(published)
-    buf.append(", ffId=").append(ffId)
-    buf.append(", userProfile=").append(userProfile)
-    buf.append("]").toString
-  }
-}
-
-case class FriendFeedSpiderResult (val entries :Seq[FriendFeedEntry])
-  extends SpiderResult {
-  def toString (buf :StringBuffer) = {
-    buf.append("entries=").append(entries)
-  }
-}
-
 /**
  * The main crawler for fetching a user's posts to their FriendFeed feed.
  *
@@ -91,11 +22,14 @@ case class FriendFeedSpiderResult (val entries :Seq[FriendFeedEntry])
  *
  * A remote key is required for accessing private user feeds.
  */
-class FriendFeedSpider (urlFetcher :URLFetcher) extends Spider(urlFetcher) {
-  def crawl (configs :Seq[SpiderConfig]) :Seq[SpiderResult] =
-    configs.map(c => getUserPosts(c.asInstanceOf[FriendFeedSpiderConfig]))
+class FriendFeedSpider (urlFetcher :URLFetcher) extends Spider(urlFetcher)
+{
+  import FriendFeedSpider._
 
-  def getUserPosts (config :FriendFeedSpiderConfig) :SpiderResult = {
+  def crawl (configs :Seq[Spider.Config]) :Seq[Spider.Result] =
+    configs.map(c => getUserPosts(c.asInstanceOf[Config]))
+
+  def getUserPosts (config :Config) :Spider.Result = {
     // build the url to retrieve this user's entries
     val url = 
       "http://friendfeed.com/api/feed/user/" + config.username + "?format=xml"
@@ -120,36 +54,78 @@ class FriendFeedSpider (urlFetcher :URLFetcher) extends Spider(urlFetcher) {
     val entries = entryElements.map(parseEntryElement(_))
 
     // create the final spider result
-    FriendFeedSpiderResult(entries)
+    Result(entries)
   }
 
-  protected def parseEntryElement (e :Node) :FriendFeedEntry = {
+  protected def parseEntryElement (e :Node) :Entry = {
     // parse the service record
     val serviceElement = (e \ "service")
-    val service = FriendFeedService((serviceElement \ "profileUrl").text,
-                                    (serviceElement \ "iconUrl").text,
-                                    (serviceElement \ "entryType").text,
-                                    (serviceElement \ "name").text)
+    val service = Service((serviceElement \ "profileUrl").text,
+                          (serviceElement \ "iconUrl").text,
+                          (serviceElement \ "entryType").text,
+                          (serviceElement \ "name").text)
 
     // parse the user profile record
     val userElement = (e \ "user")
-    val profile = FriendFeedProfile((userElement \ "profileUrl").text,
-                                    (userElement \ "nickname").text,
-                                    (userElement \ "id").text,
-                                    (userElement \ "name").text)
+    val profile = Profile((userElement \ "profileUrl").text,
+                          (userElement \ "nickname").text,
+                          (userElement \ "id").text,
+                          (userElement \ "name").text)
 
     // construct the overall entry record for this post
-    FriendFeedEntry(_dateFormat.parse((e \ "updated").text),
-                    service,
-                    (e \ "title").text,
-                    (e \ "link").text,
-                    _dateFormat.parse((e \ "published").text),
-                    (e \ "id").text,
-                    profile)
+    Entry(_dateFormat.parse((e \ "updated").text),
+          service,
+          (e \ "title").text,
+          (e \ "link").text,
+          _dateFormat.parse((e \ "published").text),
+          (e \ "id").text,
+          profile)
   }
 
   /** Date format parser for FriendFeed dates which are in RFC 3339 format. */
   private[this] val _dateFormat = new SimpleDateFormat("yyyy-mm-dd'T'HH:mm:ss'Z'")
+}
+
+object FriendFeedSpider
+{
+  /** Details a single user's FriendFeed configuration. */
+  class Config extends Spider.Config {
+    /** The FriendFeed account's username. */
+    def username () :String = reqA(stringM, "username").data
+
+    /** The FriendFeed account's remote key. */
+    def remoteKey () :String = reqA(stringM, "remoteKey").data
+  }
+
+  /** Describes a service for which posts may be seen in a user's FriendFeed feed. */
+  case class Service (val profileUrl :String,
+                      val iconUrl :String,
+                      val entryType :String,
+                      val name :String)
+
+  /** Describes a FriendFeed user's profile. */
+  case class Profile (val profileUrl :String,
+                      val nickname :String,
+                      val ffId :String,
+                      val name :String)
+
+  /** Describes a single post in a user's FriendFeed feed. */
+  // TODO: anonymous, hidden, id(internal)
+  case class Entry (val updated :Date, 
+                    val service :Service,
+                    val title :String,
+                    val link :String,
+                    val published :Date,
+                    val ffId :String,
+                    val userProfile :Profile)
+
+  case class Result (val entries :Seq[Entry]) extends Spider.Result
+
+  def configBuilder () = new Spider.ConfigBuilder {
+    def username (username :String) = { add("username", username); this }
+    def remoteKey (remoteKey :String) = { add("remoteKey", remoteKey); this }
+    def build :FriendFeedSpider.Config = build(new FriendFeedSpider.Config)
+  }
 }
 
 object FriendFeedSpiderApp {
@@ -163,7 +139,7 @@ object FriendFeedSpiderApp {
     val remoteKey = if (args.length > 1) args(1) else null
 
     // construct the user list to be queried
-    val configs = List(FriendFeedSpiderConfig(username, remoteKey))
+    val configs = List(FriendFeedSpider.configBuilder.username(username).remoteKey(remoteKey).build)
 
     // query friendfeed for the latest posts
     val crawler = new FriendFeedSpider(new URLFetcher)
--- src/scala/com/twingle/spider/Spider.scala
+++ src/scala/com/twingle/spider/Spider.scala
@@ -3,6 +3,32 @@
 
 package com.twingle.spider
 
+import com.twingle.persist.DatabaseObject
+
 abstract class Spider (val urlFetcher :URLFetcher) {
-  def crawl (configs :Seq[SpiderConfig]) :Seq[SpiderResult]
+  import Spider._
+
+  def crawl (configs :Seq[Spider.Config]) :Seq[Spider.Result]
+}
+
+/**
+ * Spider utility methods.
+ */
+object Spider
+{
+  /** Defines the configuration elements common to all spiders. */
+  abstract class Config extends DatabaseObject {
+    /** Whether or not this spider is enabled. */
+    def enabled () :Boolean = reqA(booleanM, "enabled").data
+
+    /** The frequency (in seconds) at which we run this spider. */
+    def runEvery () :Int = reqA(intM, "runEvery").data
+  }
+
+  abstract class Result
+
+  abstract class ConfigBuilder extends DatabaseObject.Builder {
+    def enabled (enabled :Boolean) :this.type = { add("enabled", enabled); this }
+    def runEvery (runEvery :Int) :this.type = { add("runEvery", runEvery); this }
+  }
 }
