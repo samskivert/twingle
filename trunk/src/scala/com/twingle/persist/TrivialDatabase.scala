e508712de85466d5a094771bebfe573d11488be5
samskivert <samskivert@47fd925a-9d4c-11dd-87e9-a30ea5446288>
2009-04-26 22:36:26 +0000

    Added support for obtaining metadata about our database object classes, use that metadata to implement brute force searching in trivial database.

--- trunk/src/scala/com/twingle/persist/TrivialDatabase.scala
+++ trunk/src/scala/com/twingle/persist/TrivialDatabase.scala
@@ -24,18 +24,45 @@ class TrivialDatabase extends AnyRef with Database
   }
 
   // from Database
-  def find (query :String) :List[DatabaseObject] = {
-    Nil // TODO
+  def find (query :String) :Iterator[DatabaseObject] = {
+    _db.values.filter(obj => matches(obj, query))
   }
 
   // from Database
-  def find (id :UUID) :List[DatabaseObject] = {
-    Nil // TODO
+  def find (id :UUID) :Iterator[DatabaseObject] = {
+    _db.values.filter(obj => matches(obj, id))
   }
 
-//   // from Database
-//   def make (data :Map[String, String]) :DatabaseObject = {
-//   }
+  protected def matches (obj :DatabaseObject, query :String) = {
+    val meta = DatabaseObject.meta(obj)
+    val tquery = new TrivialDatabase.Query(query)
+    meta.stringAttrs.exists(m => m.invoke(obj) match {
+      case text :String => tquery.matches(text)
+      case None => false
+      case Some(text) => tquery.matches(text.asInstanceOf[String])
+      case tlist :List[_] => tlist.exists(text => tquery.matches(text.asInstanceOf[String]))
+      case _ => false
+    })
+  }
+
+  protected def matches (obj :DatabaseObject, qid :UUID) = {
+    val meta = DatabaseObject.meta(obj)
+    meta.uuidAttrs.exists(m => m.invoke(obj) match {
+      case id :UUID => id == qid
+      case None => false
+      case Some(id) => id.asInstanceOf[UUID] == qid
+      case tlist :List[_] => tlist.exists(id => id.asInstanceOf[UUID] == qid)
+      case _ => false
+    })
+  }
 
   protected val _db = Map[UUID, DatabaseObject]()
+}
+
+object TrivialDatabase
+{
+  class Query (query :String) {
+    def matches (text :String) = _tokens.exists(tok => (text.indexOf(tok) != -1))
+    private[this] val _tokens = query.split("\\s")
+  }
 }
