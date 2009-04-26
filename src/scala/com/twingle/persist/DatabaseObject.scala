467bda92ba6c3ad1c925ad6e2169edba9c86a4aa
samskivert <samskivert@47fd925a-9d4c-11dd-87e9-a30ea5446288>
2009-04-26 22:36:26 +0000

    Added support for obtaining metadata about our database object classes, use that metadata to implement brute force searching in trivial database.

--- src/scala/com/twingle/persist/DatabaseObject.scala
+++ src/scala/com/twingle/persist/DatabaseObject.scala
@@ -3,6 +3,8 @@
 
 package com.twingle.persist
 
+import java.lang.reflect.Method
+import java.lang.reflect.ParameterizedType
 import java.nio.ByteBuffer
 import java.util.Date
 import java.util.UUID
@@ -18,21 +20,20 @@ trait DatabaseObject
   /** This object's unique identifier. */
   def id :UUID = reqA(uuidM, "id").data
 
-  /**
-   * Returns the names of all attributes contained in this object.
-   */
+  /** Returns the names of all attributes contained in this object. */
   def attrs () :Iterator[String] = _attrs.keys
 
-  /**
-   * Returns true if this object contains an attribute with the specified name.
-   */
+  /** Returns true if this object contains an attribute with the specified name. */
   def hasAttr (name :String) :Boolean = _attrs.contains(name)
+
+  /** Returns a brief string identifier for this instance. */
+  def idString = getClass.getSimpleName + ":" + id
 
   // from AnyRef
   override def toString = _attrs.toString
 
   /**
-   * Returns the string value of the supplied field.
+   * Returns the string value of the supplied attr.
    */
   protected def value (name :String) :Option[String] = _attrs.get(name)
 
@@ -44,17 +45,14 @@ trait DatabaseObject
     /** The data for this attribute, wrapped in its container. */
     def data :Container
 
-    /** Returns the data for this attribute or the supplied default if attr is not defined. */
-    def | (default: T) :T
+    /** Returns the marshaller used by this attribute. */
+    private[persist] def marshaller = marsh
   }
 
   /** Represents a required attribute. */
   protected class ReqAttr[T] (marsh :Marshaler[T], name :String) extends Attr[T](marsh, name) {
     /** Required attributes have no container as they always exist. */
     type Container = T
-
-    /** Returns the data for this attribute. Throws an exception if it does not exist. */
-    def | (default :T) :T = data
 
     // from Attr
     override def data :T = marsh.unmarshal(value(name).get)
@@ -67,9 +65,6 @@ trait DatabaseObject
   protected class OptAttr[T] (marsh :Marshaler[T], name :String) extends Attr[T](marsh, name) {
     /** Define our container type. In this case an Option. */
     type Container = Option[T]
-
-    /** Returns the value of this attribute or the supplied default. */
-    def | (default :T) :T = data.getOrElse(default)
 
     // from Attr
     override def data :Option[T] = value(name) match {
@@ -85,9 +80,6 @@ trait DatabaseObject
   protected class ListAttr[T] (marsh :Marshaler[T], name :String) extends Attr[T](marsh, name) {
     /** List attribute data is wrapped in a List. */
     type Container = List[T]
-
-    /** Returns the head of our list or the default if the list is empty. */
-    def | (default :T) :T = if (data.isEmpty) default else data.head
 
     def data :List[T] = value(name) match {
       case None => Nil
@@ -145,9 +137,46 @@ object DatabaseObject
     }
 
     private[this] def marsh[T <: Any] (value :T) =
-      marshalers.get(value.asInstanceOf[AnyRef].getClass).get.asInstanceOf[Marshaler[T]]
+      _marshalers.get(value.asInstanceOf[AnyRef].getClass).get.asInstanceOf[Marshaler[T]]
 
     private[this] var _map = Map[String, String]()
+  }
+
+  class Metadata[T <: DatabaseObject] (clazz :Class[T]) {
+    /** Returns all attributes of the object that contain UUIDs. */
+    val uuidAttrs = clazz.getMethods.filter(m => isType(m, classOf[UUID]))
+
+    /** Returns all attributes of the object that contain Strings. */
+    val stringAttrs = clazz.getMethods.filter(m => isType(m, classOf[String]))
+
+    /** Returns all attributes of the object that contain Dates. */
+    val dateAttrs = clazz.getMethods.filter(m => isType(m, classOf[Date]))
+
+// TODO: nix this if we decide to nix the raw attr getter methods
+//     private[this] def attrGetter (method :Method) = method.getName().endsWith("_")
+
+    private[this] def isType (method :Method, target :Class[_]) = method.getGenericReturnType match {
+      case clazz :Class[_] => (clazz == target)
+      case ptype :ParameterizedType => (ptype.getActualTypeArguments.length == 1 &&
+                                        ptype.getActualTypeArguments.apply(0) == target &&
+                                        (ptype.getRawType == classOf[Option[_]] ||
+                                         ptype.getRawType == classOf[List[_]]))
+      case _ => false
+    }
+  }
+
+  /** Returns the metadata for the supplied object. */
+  def meta[T <: DatabaseObject] (obj :T) :Metadata[T] = meta(obj.getClass.asInstanceOf[Class[T]])
+
+  /** Returns the metadata for objects of the supplied class. */
+  // TODO: can we do this with getOrElseUpdate and some type jockeying?
+  def meta[T <: DatabaseObject] (clazz :Class[T]) :Metadata[T] = _metadb.get(clazz) match {
+    case Some(meta) => meta.asInstanceOf[Metadata[T]]
+    case None => {
+      val meta :Metadata[T] = new Metadata(clazz)
+      _metadb += (clazz -> meta)
+      meta
+    }
   }
 
   /** Used to convert strings to and from target data types. */
@@ -159,47 +188,49 @@ object DatabaseObject
     def unmarshal (value :String) :T
   }
 
-  /** Creates a marshaler for {@link UUID} fields. */
+  /** Creates a marshaler for {@link UUID} attrs. */
   protected val uuidM :Marshaler[UUID] = new Marshaler[UUID] {
     def marshal (value :UUID) = value.toString
     def unmarshal (value :String) = UUID.fromString(value)
   }
 
-  /** Creates a marshaler for Boolean fields. */
+  /** Creates a marshaler for Boolean attrs. */
   protected val booleanM :Marshaler[Boolean] = new Marshaler[Boolean] {
     def marshal (value :Boolean) = value.toString
     def unmarshal (value :String) = java.lang.Boolean.parseBoolean(value)
   }
 
-  /** Creates a marshaler for String fields. */
+  /** Creates a marshaler for String attrs. */
   protected val stringM :Marshaler[String] = new Marshaler[String] {
     def marshal (value :String) = value
     def unmarshal (value :String) = value
   }
 
-  /** Creates a marshaler for Int fields. */
+  /** Creates a marshaler for Int attrs. */
   protected val intM :Marshaler[Int] = new Marshaler[Int] {
     def marshal (value :Int) = value.toString
     def unmarshal (value :String) = java.lang.Integer.parseInt(value)
   }
 
-  /** Creates a marshaler for {@link Date} fields. */
+  /** Creates a marshaler for {@link Date} attrs. */
   protected val dateM :Marshaler[Date] = new Marshaler[Date] {
     def marshal (value :Date) = value.getTime.toString
     def unmarshal (value :String) = new Date(java.lang.Long.parseLong(value))
   }
 
-  /** Creates a marshaler for {@link ByteBuffer} fields. */
+  /** Creates a marshaler for {@link ByteBuffer} attrs. */
   protected val byteBufferM :Marshaler[ByteBuffer] = new Marshaler[ByteBuffer] {
     def marshal (value :ByteBuffer) = new String(value.array, "ISO-8859-1")
     def unmarshal (value :String) = ByteBuffer.wrap(value.getBytes("ISO-8859-1"))
   }
 
-  protected val marshalers = scala.collection.mutable.Map[Class[_], Marshaler[_]]()
-  marshalers += (classOf[UUID] -> uuidM)
-  marshalers += (classOf[Boolean] -> booleanM)
-  marshalers += (classOf[String] -> stringM)
-  marshalers += (classOf[Int] -> intM)
-  marshalers += (classOf[Date] -> dateM)
-  marshalers += (classOf[ByteBuffer] -> byteBufferM)
+  protected val _marshalers = scala.collection.mutable.Map[Class[_], Marshaler[_]]()
+  _marshalers += (classOf[UUID] -> uuidM)
+  _marshalers += (classOf[Boolean] -> booleanM)
+  _marshalers += (classOf[String] -> stringM)
+  _marshalers += (classOf[Int] -> intM)
+  _marshalers += (classOf[Date] -> dateM)
+  _marshalers += (classOf[ByteBuffer] -> byteBufferM)
+
+  protected val _metadb = scala.collection.mutable.Map[Class[_], DatabaseObject.Metadata[_]]()
 }
