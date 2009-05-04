007c1c9684feefbf655e51cc5fc73d8271143f59
samskivert <samskivert@47fd925a-9d4c-11dd-87e9-a30ea5446288>
2009-05-04 03:36:13 +0000

    OMFGRLY?

--- src/scala/com/twingle/persist/DerbyDatabase.scala
+++ src/scala/com/twingle/persist/DerbyDatabase.scala
@@ -4,6 +4,9 @@
 package com.twingle.persist
 
 import java.sql.DriverManager
+import java.sql.SQLException
+
+import com.twingle.Log.log
 
 /**
  * Provides a database implementation backed by the Derby Java SQL database.
@@ -14,7 +17,16 @@ class DerbyDatabase extends JDBCDatabase
   override def shutdown () {
     super.shutdown()
     // this is how derby is shutdown, krazy!
-    DriverManager.getConnection("jdbc:derby:;shutdown=true")
+    try {
+      DriverManager.getConnection("jdbc:derby:;shutdown=true")
+    } catch {
+      case se :SQLException => 
+        if (se.getErrorCode() == 50000 && se.getSQLState() == "XJ015") {
+          // everything's AOK
+        } else {
+          log.warning("Failed to shutdown Derby", se)
+        }
+    }
   }
 
   protected def driverClass = "org.apache.derby.jdbc.EmbeddedDriver"
